16dd5cbaf71d3d9730112cb308756dd99330012c
William Reese Sexton <reese.sexton01@gmail.com>
2018-07-15 18:46:33 -0400

    Added onEnter event for faq dropdowns.  Need to fix tab order...

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,12 +1,16 @@
-$(document).ready(function() {
-  // Hide all faq answers by default
-  $("#faq li .faq-ans").each(function(i) {
-    $(this).hide();
-  });
-  // Toggle visibility of faq answers when questions are clicked
-  $("#faq li .faq-q").each(function(i) {
-    $(this).click(function() {
-      $(this).parent().children('.faq-ans').slideToggle(400);
+//
+const showAns = ans => ans.slideToggle(400);
+
+// Hide all faq answers by default
+$("#faq li .faq-ans").each(function(i) {
+  $(this).hide();
+});
+// Toggle visibility of faq answers when questions are clicked
+$("#faq li .faq-q").each(function(i) {
+  const $faqAns = $(this).parent().children('.faq-ans');
+  $(this).click(() => showAns($faqAns));
+  $(this).keypress(function (ev) {
+        const keycode = (ev.keyCode ? ev.keyCode : ev.which);
+        if (keycode == '13') showAns($faqAns);
     });
-  });
 });
